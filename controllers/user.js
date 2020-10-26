f438c15193f510f4f8e00faed88209a5cfc38713
TKVAIOZBLK\miyuk_000 <scrambleegg7@gmail.com>
2020-10-26 15:38:14 +0900

    signin signup signout

--- controllers/user.js
+++ controllers/user.js
@@ -63,3 +63,12 @@ exports.signin = (req, res) => {
    )
 }
 
+exports.signout = (req, res) => {
+
+    res.clearCookie("t");
+    res.json({
+        message: "Signout success."
+    });
+
+
+}
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { signin, signup, sayHi } = require('../controllers/user');
+const { signin, signup, signout, sayHi } = require('../controllers/user');
 const { userSignUpValidator } = require('../validator/index');
 
 router.post("/signup", userSignUpValidator,  signup);
 router.post("/signin",  signin);
+router.get("/signout",  signout);
 
 router.get("/", sayHi);
 
